46cd72bcb456c85b71dd4149e57c751b99d724b1
Alex <Cheb@Alex.ru>
2024-07-03 22:32:02 +0300

    upd varibles name

--- RectangleTask/RectangleTask/Program.cs
+++ RectangleTask/RectangleTask/Program.cs
@@ -11,14 +11,14 @@ using (var logs = MainLog.CreateAllLog())
     var secondRedRectangle = new ColorRectangle(ConsoleColor.Red, new Point(3, 3), 1, 1);
     var secondBlueRectangle = new ColorRectangle(ConsoleColor.Blue, new Point(2, 2), 1, 1);
     var secondRedRectangle2 = new ColorRectangle(ConsoleColor.Red, new Point(3, 1), 3, 2);
-    var secondBlueRectangle2 = new ColorRectangle(ConsoleColor.Red, new Point(8, 2), 1, 1);
+    var secondRedRectangle3 = new ColorRectangle(ConsoleColor.Red, new Point(8, 2), 1, 1);
 
     mainRectangle.AddRectangles(
        new[]{
                         secondRedRectangle,
                         secondBlueRectangle,
                         secondRedRectangle2,
-                        secondBlueRectangle2
+                        secondRedRectangle3
             }
         );
 
